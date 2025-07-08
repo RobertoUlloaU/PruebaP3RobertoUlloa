afb4116bc9dea9eb915ac725480d3580aaccd28d
UlloaRoberto <roberto.ulloa@udla.edu.ec>
2025-07-08 07:23:20 -0500

    Creamos DatabaseHelper que manejara la conexion a la base de datos SQLite

---
+++ PruebaP3RobertoUlloa/Models/DatabaseHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace PruebaP3RobertoUlloa.Models
+{
+    
+
+    public class DatabaseHelper
+    {
+        private readonly SQLiteConnection _database;
+
+        public DatabaseHelper(string dbPath)
+        {
+            _database = new SQLiteConnection(dbPath);
+            _database.CreateTable<Dispositivo>();
+        }
+
+        public int SaveDispositivo(Dispositivo dispositivo)
+        {
+            if (dispositivo.Id != 0)
+            {
+                return _database.Update(dispositivo);
+            }
+            else
+            {
+                return _database.Insert(dispositivo);
+            }
+        }
+
+        public List<Dispositivo> GetDispositivos()
+        {
+            return _database.Table<Dispositivo>().ToList();
+        }
+    }
+
+}
---
+++ PruebaP3RobertoUlloa/Models/Dispositivo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaP3RobertoUlloa.Models
+{
+    public class Dispositivo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+        public bool GarantiaActiva { get; set; }
+        public int VidaUtilMeses { get; set; }
+    }
+
+}
