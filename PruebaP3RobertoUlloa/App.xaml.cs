daef26b5330fad1bfd2e6cf3cb6accf15623b06d
UlloaRoberto <roberto.ulloa@udla.edu.ec>
2025-07-08 07:50:19 -0500

    Implementacion de la lógica de la pagina principal para guardar los dispositivos en la base de datos y generar el archivo de log

--- PruebaP3RobertoUlloa/App.xaml.cs
+++ PruebaP3RobertoUlloa/App.xaml.cs
@@ -3,10 +3,16 @@ using System.IO;
 using Microsoft.Maui.Storage;
 using PruebaP3RobertoUlloa.Models;
 
+using Microsoft.Maui.Controls;
+using System;
+
 namespace PruebaP3RobertoUlloa
 {
-    public partial class App : Application
+    public partial class MainPage : ContentPage
     {
-        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
     }
 }
